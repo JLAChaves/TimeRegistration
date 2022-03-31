8c16c5222b34d811eb668f3a8f427685972f5af1
JLAChaves <lucas2015nxi@gmail.com>
2022-03-31 14:19:42 -0400

    Models Contract and TimeLog created

---
+++ TimeRegistration/Models/Contract.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeRegistration.Models
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double? ValuePerHour { get; set; }
+        public double? TotalValue { get; set; }
+        public double TotalHours { get; set; }
+        public ICollection<TimeLog>? TimeLogs { get; set; }
+    }
+}
---
+++ TimeRegistration/Models/TimeLog.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeRegistration.Models
+{
+    public class TimeLog
+    {
+        public int Id { get; set; }
+        public int? ContractId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Hours { get; set; }
+    }
+}
