20744bb7334b8afd52c65ab9d06d838ab05ddabe
JLAChaves <lucas2015nxi@gmail.com>
2022-03-31 14:29:17 -0400

    Created the Data folder that contains the DbContext

---
+++ TimeRegistration/Data/TimeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeRegistration.Models;
+
+namespace TimeRegistration.Data
+{
+    public class TimeContext : DbContext
+    {
+        public TimeContext(DbContextOptions<TimeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<TimeLog> TimesLogs { get; set; }
+    }
+}
