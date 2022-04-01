7caff2e8a4c93c96076e30a30fcb8d59a53111b0
JLAChaves <lucas2015nxi@gmail.com>
2022-04-01 17:04:53 -0400

    TimeLogController created with basic CRUD

---
+++ TimeRegistration/Controllers/TimeLogController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeRegistration.Models;
+using TimeRegistration.Repositories;
+
+namespace TimeRegistration.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class TimeLogController : ControllerBase
+    {
+        private readonly ITimeLogRepository _repo;
+
+        public TimeLogController(ITimeLogRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        public IActionResult Create(TimeLog timeLog)
+        {
+            if (_repo.Create(timeLog))
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var getAll = _repo.ReadAll();
+                return Ok(getAll);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPut("UpdateExit/{id}")]
+        public IActionResult UpdateExit(int id)
+        {
+            if (_repo.UpdateExit(id))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(TimeLog timeLog)
+        {
+            if (_repo.Delete(timeLog))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
